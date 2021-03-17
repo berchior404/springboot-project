b999d50e8920db4a3b1794a300d5c5ab393a488a
Pedro H B Cardoso <pedro_berchior@outlook.com>
2021-03-17 01:02:55 -0300

    Product

--- src/main/java/com/pdev/course/entities/Category.java
+++ src/main/java/com/pdev/course/entities/Category.java
@@ -1,12 +1,15 @@
 package com.pdev.course.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
@@ -15,9 +18,12 @@ public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
 	private Long id;
 	private String name;
+	
+	@Transient
+	private Set<Product> products = new HashSet<>();
 	
 	public Category() {}
 
@@ -41,6 +47,10 @@ public class Category implements Serializable {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public Set<Product> getProducts() {
+		return products;
 	}
 
 	@Override
---
+++ src/main/java/com/pdev/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.pdev.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pdev.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
