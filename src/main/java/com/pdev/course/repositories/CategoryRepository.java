73d106f5d90f2c15d44a773ef2317936af0b287a
Pedro H B Cardoso <pedro_berchior@outlook.com>
2021-03-17 00:37:01 -0300

    Category

---
+++ src/main/java/com/pdev/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.pdev.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pdev.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
---
+++ src/main/java/com/pdev/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.pdev.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pdev.course.entities.Category;
+import com.pdev.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
