6fc6054bca5f93d6f53b2b2d3a87bd7e1458f534
Pedro H B Cardoso <pedro_berchior@outlook.com>
2021-03-20 18:19:51 -0300

    Payment, one-to-one association

--- src/main/java/com/pdev/course/entities/OrderItem.java
+++ src/main/java/com/pdev/course/entities/OrderItem.java
@@ -12,6 +12,7 @@ import com.pdev.course.entities.pk.OrderItemPK;
 @Entity
 @Table(name = "tb_order_item")
 public class OrderItem implements Serializable{
+	
 	private static final long serialVersionUID = 1L;
 	
 	@EmbeddedId
